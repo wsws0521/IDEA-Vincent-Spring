4135b6c05dc726a431a047f8e7fc4bafdd7ae383
wsws0521 <623540439@qq.com>
2020-09-10 11:29:02 +0200

    action实战

---
+++ springboot-action/src/main/java/cn/vincent/action/springbootaction/SpringbootActionApplication.java
@@ -0,0 +1,13 @@
+package cn.vincent.action.springbootaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActionApplication.class, args);
+    }
+
+}
---
+++ springboot-action/src/test/java/cn/vincent/action/springbootaction/SpringbootActionApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.vincent.action.springbootaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
